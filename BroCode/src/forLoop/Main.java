8ef9bb7144d7d75bfb7389be3f01a2d77f65a78b
vignesh <vigneshvitsmeon@gmail.com>
2022-03-19 23:23:49 +0530

    java practice3

---
+++ BroCode/src/forLoop/Main.java
@@ -0,0 +1,21 @@
+package forLoop;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// for loop = executes a block of code a limited amount of times
+		
+		//for (int i=10; i>=10; i--)     //for reverse
+		//instead of i-- use i-=2  for doubling
+		for(int i=0; i<=10; i++)
+		{
+			System.out.println(i);
+			//here also we can use i++ remove from for loop   //i-=2;
+		}
+		System.out.println("Happy new year!");
+		
+
+	}
+
+}
